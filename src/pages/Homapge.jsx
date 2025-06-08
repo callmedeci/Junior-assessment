0f83c0405200a6ec0a8b63c4d11636d94ab14aa3
callmedeci <callmedecim@gmail.com>
2025-06-08 10:11:38 -0700

    [FIX] styling issues

--- src/pages/Homapge.jsx
+++ src/pages/Homapge.jsx
@@ -8,29 +8,31 @@ const auth = getAuth();
 
 function Homepage() {
   return (
-    <div className='bg-zinc-800 w-full min-w-3xl max-w-3xl min-h-96 rounded-2xl shadow-md p-5'>
-      <div className='bg-zinc-900/50 flex flex-col gap-5 rounded-2xl p-5'>
-        <div className='flex items-center'>
-          <h1 className='text-2xl font-semibold text-zinc-200 mb-2 text-center capitalize'>
-            Welcome back{' '}
-            <span className='text-indigo-500'>
-              {auth.currentUser.displayName}
-            </span>
-          </h1>
+    <main className='bg-zinc-950/50 p-5 rounded-2xl'>
+      <div className='bg-zinc-800/50 flex items-center gap-5 rounded-2xl p-5'>
+        <h1 className='text-2xl font-semibold text-zinc-200 mb-2 text-center capitalize'>
+          Welcome back{' '}
+          <span className='text-indigo-500'>
+            {auth.currentUser.displayName}
+          </span>
+        </h1>
 
-          <div className='flex flex-1 justify-end'>
-            <Avatar
-              imageSrc={auth.currentUser.photoURL}
-              alt={auth.currentUser.displayName}
-            />
-          </div>
+        <div className='flex flex-1 justify-end'>
+          <Avatar
+            imageSrc={auth.currentUser.photoURL}
+            alt={auth.currentUser.displayName}
+          />
         </div>
-
-        <ResponseForm />
       </div>
 
-      <MessagesList />
-    </div>
+      <div className='bg-zinc-800 w-full min-w-3xl max-w-3xl min-h-96 rounded-2xl shadow-md p-5 my-2'>
+        <MessagesList />
+      </div>
+
+      <div className='bg-zinc-800/75 flex items-center gap-5 rounded-2xl p-5 shadow-md'>
+        <ResponseForm />
+      </div>
+    </main>
   );
 }
 
